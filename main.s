f00b22b3d981144a694deb6b8bc2dd85b182fde2
Bodo Kaiser <i@bodokaiser.io>
2020-04-17 19:37:44 +0200

    initial commit

---
+++ main.s
@@ -0,0 +1,26 @@
+.syntax unified
+.cpu cortex-m0
+.fpu softvfp
+.thumb
+
+.global vector_table
+.global reset_handler
+
+.type vector_table, %object
+vector_table:
+    .word _estack
+    .word reset_handler
+.size vector_table, .-vector_table
+
+.type reset_handler, %function
+reset_handler:
+    LDR  r0, =_estack
+    MOV  sp, r0
+
+    LDR  r7, =0xDEADBEEF
+    MOVS r0, #0
+    
+    main_loop:
+    ADDS r0, r0, #1
+    B main_loop
+.size reset_handler, .-reset_handler
